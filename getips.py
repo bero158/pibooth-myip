2ec0e4242bd79cd1ccd0d825660e01226a97d2de
bero158 <tomas.beran@gmail.com>
2024-06-17 19:43:08 +0200

    Update getips.py
    sanitized ifaces

--- getips.py
+++ getips.py
@@ -6,17 +6,18 @@ except ImportError:
 
 def getIPList():
     ifaces=netifaces.interfaces()
-    ifaces.remove("lo")
-    addrList = []
-    for iface in ifaces:
-        ifcAddresses = netifaces.ifaddresses(iface)
-        if ifcAddresses:
-            ifcInetAddresses = ifcAddresses.get(netifaces.AF_INET)
-            if ifcInetAddresses:
-                for inetAddr in ifcInetAddresses:
-                    addr = inetAddr.get("addr")
-                    if addr:
-                        addrList.append(addr)
+    if ifaces:
+        ifaces.remove("lo")
+        addrList = []
+        for iface in ifaces:
+            ifcAddresses = netifaces.ifaddresses(iface)
+            if ifcAddresses:
+                ifcInetAddresses = ifcAddresses.get(netifaces.AF_INET)
+                if ifcInetAddresses:
+                    for inetAddr in ifcInetAddresses:
+                        addr = inetAddr.get("addr")
+                        if addr:
+                            addrList.append(addr)
     return addrList
 
 def getIPStr(addrList : list = None, sep : str = ", "):
